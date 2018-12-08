17025801099d6bd1d0caf7f3147f92e6d973861b
jpeng304 <jpeng304@hotmail.com>
2018-12-08 00:45:12 -0800

    write.csv() for CrimeTypePerLocation and CrimePerYear

--- src/main/java/edu/ucr/cs/cs226/groupF/CrimeAnalyze.java
+++ src/main/java/edu/ucr/cs/cs226/groupF/CrimeAnalyze.java
@@ -2,12 +2,14 @@ package edu.ucr.cs.cs226.groupF;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-
+import org.apache.spark.sql.*;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.apache.spark.sql.functions.*;
 
@@ -15,20 +17,31 @@ public class CrimeAnalyze {
 
     public static void main(String[] args){
 
-        List<String> rows = new ArrayList<>();
         Logger.getLogger("org").setLevel(Level.ERROR);
+        String file_path = "file:///home/jonathan/";
 
         SparkSession session = SparkSession.builder().appName("CrimeSolution").master("local[*]").getOrCreate();
 
         //when reading the csv file to the dataset as rows, we set the read in delimeter as tab since we use tab to
         //seperate each column and set the let the first row of the csv file as the schema.
-        Dataset<Row> CombineData = session.read().option("delimiter","\t").option("header", "true").csv("/home/jonathan/Chicago_Crimes.csv");
+        Dataset<Row> CombineData = session.read().option("delimiter","\t").option("header", "true").csv(file_path+"Chicago_Crimes.csv");
 
 
         //CombineData.printSchema();
+/*
+        CombineData.createOrReplaceTempView("CombineData");
+        Dataset<Row> fa = session.sqlContext().sql("Select `Primary Type` From CombineData ");
+        fa.write().format("com.databricks.spark.csv").option("header", "true").save("/home/jonathan/csv");
+        //fa.groupBy("Primary Type").count().show();
+*/
+        //System.out.println("The Type of crime for the total year");
 
-        System.out.println("The Type of crime for the total year");
+
+
         CombineData.groupBy("Primary Type").count().orderBy(desc("count")).show(40, false);
+        //Dataset<Row> ab  = CombineData.groupBy("Primary Type").count().orderBy("count");
+            //ab.write().format("com.databricks.spark.csv").option("header", "true").save("/home/jonathan/csv");
+        //ab.coalesce(2).write().option("header", "true").csv("/home/jonathan/csv1");
 
 
 
@@ -39,29 +52,29 @@ public class CrimeAnalyze {
 
         Dataset<Row> filterData = CombineData.withColumn("Year" ,col("Year").cast("integer"));
         //System.out.println("Print the filter schema");
-        //filterData.printSchema();
-        //System.out.println("Crime each year without null");
-        //Dataset<Row> filterData = CombineData.select("Year").filter("Year != 'True' and Year != 'False'");
+            //filterData.printSchema();
+            //System.out.println("Crime each year without null");
+            //Dataset<Row> filterData = CombineData.select("Year").filter("Year != 'True' and Year != 'False'");
 
 
         
-        System.out.println("Print out number of arrest for each year");
+        //System.out.println("Print out number of arrest for each year");
         //filterData.groupBy("Arrest","Year").count().show();
-        filterData.where("Arrest == 'True'").groupBy("Year").count().orderBy("Year").show();
+        //filterData.where("Arrest == 'True'").groupBy("Year").count().orderBy("Year").show();
 
         System.out.println("Print out the number of crimes type for each year");
-        filterData.groupBy("Primary Type", "Year").count().orderBy("Year", "Primary Type").show(600,false);
+        Dataset<Row> CrimePerYear = filterData.groupBy("Primary Type", "Year").count().orderBy("Year", "Primary Type");
+        CrimePerYear.coalesce(1).write().option("header","true").csv(file_path + "groupF/CrimePerYear");
 
 
         System.out.println("Print out the frequent location that crime occur");
-        filterData.groupBy("Location Description").count().orderBy("count").show(500,false);
+        filterData.groupBy("Location Description").count().orderBy(desc("count")).show(200,false);
 
 
 
         System.out.println("Calculate the number of crime x base on location ");
-        filterData.groupBy("Primary Type", "Location Description").count().sort("Primary Type", "Location Description").show
-                (2500,false);
-
+        Dataset<Row> CrimePerLoc = filterData.groupBy("Primary Type", "Location Description").count().sort("Primary Type", "Location Description");
+        CrimePerLoc.coalesce(1).write().option("header","true").csv(file_path + "groupF/CrimePerLoc");
 
 
         System.out.println("Amount of Theft each year");
@@ -72,10 +85,10 @@ public class CrimeAnalyze {
         filterData.filter(filterData.col("Primary Type").equalTo("HOMICIDE")).groupBy("Year").count().orderBy("Year").show();
 
         System.out.println("Amount of Crim sexual assualt + Sex Offense each year");
-        filterData.filter(filterData.col("Primary Type").equalTo("CRIM SEXUAL ASSAULT")).union(filterData.filter(filterData.col("Primary Type").equalTo("SEX OFFENSE")))
-                .groupBy("Primary Type","Year").count().orderBy("Primary Type", "Year").show(40,false);
+        Dataset<Row> offense = filterData.filter(filterData.col("Primary Type").equalTo("CRIM SEXUAL ASSAULT")).union(filterData.filter(filterData.col("Primary Type").equalTo("SEX OFFENSE")))
+                .groupBy("Primary Type","Year").count().orderBy("Primary Type", "Year");
 
-
+        offense.coalesce(1).write().option("header","true").csv(file_path + "groupF/offense");
 
 
         //===== old data=====//
