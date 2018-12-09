bdee9f5fde7fc364acd5f273640161b4d2fb3e5f
jpeng304 <jpeng304@hotmail.com>
2018-12-09 01:33:47 -0800

    show some result and added the execution time

--- src/main/java/edu/ucr/cs/cs226/groupF/CrimeAnalyze.java
+++ src/main/java/edu/ucr/cs/cs226/groupF/CrimeAnalyze.java
@@ -20,7 +20,14 @@ public class CrimeAnalyze {
         Logger.getLogger("org").setLevel(Level.ERROR);
         String file_path = "file:///home/jonathan/";
 
+        long startTime = System.currentTimeMillis();
+        long endTime, totalTime;
+        //SparkSession session = SparkSession.builder().appName("CrimeSolution").getOrCreate();
         SparkSession session = SparkSession.builder().appName("CrimeSolution").master("local[*]").getOrCreate();
+
+        //String file_path = "file:///opt/hadoop/home/hadoop7/";
+
+        //SparkSession session = SparkSession.builder().appName("CrimeSolution").getOrCreate();
 
         //when reading the csv file to the dataset as rows, we set the read in delimeter as tab since we use tab to
         //seperate each column and set the let the first row of the csv file as the schema.
@@ -58,23 +65,24 @@ public class CrimeAnalyze {
 
 
         
-        //System.out.println("Print out number of arrest for each year");
-        //filterData.groupBy("Arrest","Year").count().show();
-        //filterData.where("Arrest == 'True'").groupBy("Year").count().orderBy("Year").show();
+        System.out.println("Print out number of arrest for each year");
+        filterData.where("Arrest == 'True'").groupBy("Year").count().orderBy("Year").show(false);
 
         System.out.println("Print out the number of crimes type for each year");
         Dataset<Row> CrimePerYear = filterData.groupBy("Primary Type", "Year").count().orderBy("Year", "Primary Type");
         CrimePerYear.coalesce(1).write().option("header","true").csv(file_path + "groupF/CrimePerYear");
-
+        filterData.groupBy("Primary Type", "Year").count().orderBy("Year", "Primary Type").show(false);
 
         System.out.println("Print out the frequent location that crime occur");
         filterData.groupBy("Location Description").count().orderBy(desc("count")).show(200,false);
-
+        Dataset<Row> freqLoc = filterData.groupBy("Location Description").count().orderBy(desc("count"));
+        freqLoc.coalesce(1).write().option("header","true").csv(file_path + "groupF/FreqLoc");
 
 
         System.out.println("Calculate the number of crime x base on location ");
         Dataset<Row> CrimePerLoc = filterData.groupBy("Primary Type", "Location Description").count().sort("Primary Type", "Location Description");
         CrimePerLoc.coalesce(1).write().option("header","true").csv(file_path + "groupF/CrimePerLoc");
+        filterData.groupBy("Primary Type", "Location Description").count().sort("Primary Type", "Location Description").show(false);
 
 
         System.out.println("Amount of Theft each year");
@@ -84,66 +92,20 @@ public class CrimeAnalyze {
         System.out.println("Amount of Homicide each year");
         filterData.filter(filterData.col("Primary Type").equalTo("HOMICIDE")).groupBy("Year").count().orderBy("Year").show();
 
-        System.out.println("Amount of Crim sexual assualt + Sex Offense each year");
+        System.out.println("Amount of Criminal Sexual Assault + Sex Offense each year");
         Dataset<Row> offense = filterData.filter(filterData.col("Primary Type").equalTo("CRIM SEXUAL ASSAULT")).union(filterData.filter(filterData.col("Primary Type").equalTo("SEX OFFENSE")))
                 .groupBy("Primary Type","Year").count().orderBy("Primary Type", "Year");
-
         offense.coalesce(1).write().option("header","true").csv(file_path + "groupF/offense");
+        offense.show(false);
 
+        endTime = System.currentTimeMillis();
 
-        //===== old data=====//
+        totalTime = (endTime - startTime)/1000;
+        double sec = totalTime%60;
+        long min = totalTime / 60;
+        System.out.println("Total execution time : " + min + " minutes and " + sec +" seconds");
 
-//        long total_crimes  = CombineData.select("*").count();
-//        System.out.println(total_crimes);
-
-/*
-        //#2
-        //System.out.println("Number of Crime Type in 2012-2017");
-        Dataset<Row> response12_17 = session.read().option("header", "true").csv("/home/jonathan/in/Chicago_Crimes_2012_" +
-                "to_2017.csv");
-        //RelationalGroupedDataset groupCrimeType12_17 = response12_17.groupBy(col("Primary Type"));
-        //groupCrimeType12_17.count().show(40,false);
-        /print the header
-        //response12_17.printSchema();
-        //#2
-        //System.out.println("Number of Crime Type in 2008-2011");
-        Dataset<Row> response08_11 = session.read().option("header", "true").csv("in/Chicago_Crimes_2008_" +
-                "to_2011.csv");
-        //RelationalGroupedDataset groupCrimeType08_11 = response08_11.groupBy(col("Primary Type"));
-        //groupCrimeType08_11.count().show(40,false);
-        //#2
-        //System.out.println("Number of Crime Type in 2005-2007");
-        Dataset<Row> response05_07 = session.read().option("header", "true").csv("in/Chicago_Crimes_2005_" +
-                "to_2007.csv");
-        //RelationalGroupedDataset groupCrimeType05_07 = response05_07.groupBy(col("Primary Type"));
-        //The truncate:false will not make the printed table all shorten
-        //need to set > 20 since function will only print 20 rows
-        //groupCrimeType05_07.count().show(40,false);
-        //#4
-        //The combine dataset
-        //System.out.println("Number of Each Crime Type in 2008-2017");
-        Dataset<Row> CombineSet = response12_17.union(response08_11);
-        //RelationalGroupedDataset groupCombineCrime = CombineSet.groupBy(col("Primary Type"));
-        //groupCombineCrime.count().show(40,false);
-        //System.out.println("The combine dataset to count how many crime type for the whole year #4==========================");
-        //CombineSet.groupBy("Primary Type").count().show(40, false);
-        //System.out.println("The combine dataset to count how many crime each year==========================");
-        //CombineSet.groupBy("Year").count().show(40, false);
-        //System.out.println("The combine dataset to count how many crime type each year #5==========================");
-        //CombineSet.groupBy("Year","Primary Type").count().sort("Year", "Primary Type").show(600, false);
-        System.out.println("Calculate how many crime base on location ");
-        CombineSet.groupBy("Location Description").count().show(40, false);
-        System.out.println("Calculate the number of crime x base on location ");
-        CombineSet.groupBy("Primary Type", "Location Description").count().sort("Primary Type").show
-                (40,false);
-        //Count the total crime in each year
-        long total_crime12_17  = response12_17.select("*").count();
-        long total_crime08_11  = response08_11.select("*").count();
-        System.out.println("The Total crime from 2012 to 2017 is : " + total_crime12_17);
-        System.out.println("The Total crime from 2008 to 2011 is : " + total_crime08_11);
-           */
-
-
+       
 
 
 
